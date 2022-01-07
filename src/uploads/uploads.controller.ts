7df1c76fbd1d98a78ee81dbacd73c2633988ae7a
PlatonSterh <platon.s14@list.ru>
2022-01-07 16:30:32 +0500

    fix: delete file after upload deletion

--- src/uploads/uploads.controller.ts
+++ src/uploads/uploads.controller.ts
@@ -10,6 +10,8 @@ import {
   Query,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { unlink } from 'fs';
+import { promisify } from 'util';
 
 import { JWTGuard } from 'src/authentication/jwt.guard';
 import { Roles } from 'src/utils/decorators/role.decorator';
@@ -17,6 +19,9 @@ import { RolesGuard } from 'src/utils/guards/roles.guard';
 import { FindUploadDto } from './dto/find-upload.dto';
 import { UploadsService } from './uploads.service';
 import { storage } from './storage.config';
+import { join } from 'path';
+
+const unlinkAsync = promisify(unlink);
 
 @Controller('uploads')
 export class UploadsController {
@@ -30,7 +35,7 @@ export class UploadsController {
     return this.uploadsService.create({
       originalName: file.originalname,
       fileName: file.filename,
-      path: process.env.API_PATH + `uploads/${file.filename}`,
+      path: `uploads/${file.filename}`,
       size: String(file.size),
       mimetype: file.mimetype,
     });
@@ -50,6 +55,12 @@ export class UploadsController {
   @Roles('admin')
   @Delete(':id')
   public async remove(@Param('id') id: string) {
+    const upload = await this.findOne(id);
+
+    await unlinkAsync(
+      join(__dirname, '..', '..', 'public', 'uploads', upload.fileName),
+    );
+
     return this.uploadsService.remove(+id);
   }
 }
