89707efc2b74a3dcc8d40f7d288ff3d37d6b0d48
PlatonSterh <platon.s14@list.ru>
2022-01-22 21:49:28 +0500

    use helmet

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { join } from 'path';
+import helmet from 'helmet';
 
 import { AppModule } from './app.module';
 
@@ -9,6 +10,8 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.use(helmet());
 
   app.enableCors();
 
